233b63cdce3e71f4266c27d42b617ba6863de36a
Livingstone Ackah <kofiackah360@gmail.com>
2024-02-14 12:06:14 +0000

    Added Images on Projects

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,9 +1,54 @@
 import React from "react";
+import { ProjectData } from "../Data/ProjectData";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 export default function Project() {
+  const slideLeft = () => {
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+
+  const slideRight = () => {
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
   return (
     <div className="md:h-screen" id="project">
-      <h2>Projects</h2>
+      {/* <div className="relative flex items-center">
+        <FontAwesomeIcon
+          icon={faBars}
+          className="opacity-50 cursor-pointer hover:opacity-100"
+          onClick={slideLeft}
+          size={40}
+        />
+        <div
+          id="slider"
+          className="w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide"
+        >
+          {ProjectData.map((item) => (
+            <img
+              className="w-[220px] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300"
+              src={item.image}
+              alt="/"
+            />
+          ))}
+        </div>
+        <FontAwesomeIcon
+          icon={faBars}
+          className="opacity-50 cursor-pointer hover:opacity-100"
+          onClick={slideRight}
+          size={40}
+        />
+      </div> */}
+      <div className="flex items-center relative">
+        <div className="overflow-x-scroll whitespace-nowrap scroll-smooth">
+          {ProjectData.map((item) => (
+            <img src={item.image} alt="project" className="inline-block" />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Data/ProjectData.js
@@ -0,0 +1,27 @@
+import HelpImage from "../Images/HelpPlus Image.png";
+import HuddleImage from "../Images/Huddle Image.png";
+import InoTechImage from "../Images/InoTech.png";
+import VegenImage from "../Images/VegenImage.png";
+
+export const ProjectData = [
+  {
+    id: 1,
+    image: HelpImage,
+    site: "https://helpplus-devlivingstone.netlify.app/",
+  },
+  {
+    id: 2,
+    image: HuddleImage,
+    site: "https://huddle-landingpage-mentor.netlify.app/",
+  },
+  {
+    id: 3,
+    image: InoTechImage,
+    site: "https://curious-crisp-8cdbd1.netlify.app/",
+  },
+  {
+    id: 4,
+    image: VegenImage,
+    site: "https://vegenfruits.netlify.app/",
+  },
+];
