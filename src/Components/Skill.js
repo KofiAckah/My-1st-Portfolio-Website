b9e1edc5463b523a804ebca4f01c7ba5e572520e
Livingstone Ackah <kofiackah360@gmail.com>
2024-02-21 02:03:31 +0000

    start adding icons to the skills page

--- src/Components/Skill.js
+++ src/Components/Skill.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHtml5, faCss3Alt, faJs } from "@fortawesome/free-brands-svg-icons";
 
 export default function Skill() {
   return (
-    <div className="md:h-screen" id="skill">
-      <p>Skills Here</p>
+    <div className="md:h-screen justify-center items-center" id="skill">
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-10">
+        <div>
+          <FontAwesomeIcon icon={faHtml5} className="h-32 md:h-44 text-white" />
+        </div>
+        <div>
+          <FontAwesomeIcon
+            icon={faCss3Alt}
+            className="h-32 md:h-44 text-white"
+          />
+        </div>
+        <div>
+          <FontAwesomeIcon icon={faJs} className="h-32 md:h-44 text-white" />
+        </div>
+      </div>
     </div>
   );
 }
