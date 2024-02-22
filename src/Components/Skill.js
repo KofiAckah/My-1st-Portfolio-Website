5bde7e5cc052c6d3bbf1b0e75c92b998c8cbfead
Livingstone Ackah <kofiackah360@gmail.com>
2024-02-22 22:55:31 +0000

    Almost done with the skill page

--- src/Components/Skill.js
+++ src/Components/Skill.js
@@ -1,24 +1,24 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faHtml5,
-  faCss3Alt,
-  faJs,
-  faReact,
-} from "@fortawesome/free-brands-svg-icons";
 import { skillData } from "../Data/SkillData";
 
 export default function Skill() {
   return (
-    <div className="md:h-screen flex flex-col" id="skill">
-      <h2 className="font-bold text-4xl md:text-6xl my-4 text-center">
+    <div
+      className="md:h-screen flex flex-col items-center justify-center"
+      id="skill"
+    >
+      <h2 className="font-bold text-4xl md:text-6xl my-4 text-center md:mb-14">
         Skills
       </h2>
-      <div className="mx-auto flex">
+      <div className="grid grid-cols-2">
         {skillData.map((item) => (
-          <div key={item.id} className="flex flex-col items-center m-4">
-            <FontAwesomeIcon icon={item.icon} />
-            <p className="text-2xl font-bold">{item.text}</p>
+          <div
+            key={item.id}
+            className={`flex items-center m-4 sm:m-6 md:m-10 changeColor delay-${item.delay} duration-[${item.duration}]`}
+          >
+            <FontAwesomeIcon icon={item.icon} className="h-6 sm:h-7" />
+            <p className="text-xl ml-2 font-bold">{item.text}</p>
           </div>
         ))}
       </div>
